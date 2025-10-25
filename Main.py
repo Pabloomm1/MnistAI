86fedb9d3fd85dcfbc93136f855269aff909b78b
pasha <cwcew@d.lr>
2025-10-25 21:42:04 +0300

    версия с ноута

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 from PIL import Image, ImageDraw
 import numpy as np
+import Perceptron1
 
 MAX_DIGXY = 20
 def str2img(pxls : str, x_off, y_off, arr_img : np.ndarray):
@@ -16,7 +17,7 @@ def str2img(pxls : str, x_off, y_off, arr_img : np.ndarray):
 
 arr = np.zeros((28 * MAX_DIGXY, 28 * MAX_DIGXY))
 dig_cnt = 0
-with open ('C:\\Users\\PAVEL\\Desktop\\project Mnist\\data\\mnist_test.csv', encoding = "ascii") as f:
+with open ('C:\Users\pusic\Desktop\MNIST_dataset\mnist\train', encoding = "ascii") as f:
     while dig_cnt != MAX_DIGXY ** 2:
         l = f.readline()
 
--- Perceptron1.py
+++ Perceptron1.py
@@ -1,1 +1,26 @@
+import numpy as np
+bias = 600
+weights = np.zeros(28 * 28)
+def enc_weights(arr: list):
+    for i in range(28 * 28):
+        weights[i] += arr[i]
+def dec_weights(arr:list):
+    for i in range(28 * 28):
+        weights[i] -= arr[i]
+def classified(arr: list) -> bool:
+    rez = 0
+    for i in range(28 * 28):
+        rez += arr[i] * weights[i]
+    return rez >= bias
+def train(positive: bool, arr: list ):
+    r = classified(arr)
+    if positive and not r:
+        enc_weights(arr) # веса ++ если нераспознано нужное нам число
+    elif not positive and r:
+        dec_weights(arr) # веса -- если распознано ненужное число
+
+
+
+
+
 
